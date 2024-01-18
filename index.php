1dc96c72f5bbf5a46a3ec82f64b3d475d41186ca
roys89 <s.sdr.roy89@gmial.com>
2024-01-19 00:30:39 +0530

    2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 // Check if the user is already logged in
 if (isset($_SESSION['user_id'])) {
-  header("Location: dashboard.php");
+  header("Location: user-profile.php");
   exit();
 }
 include "database.php";
