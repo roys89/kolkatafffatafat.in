5b5448b2afa7558b32285009f00aeca3491dd746
roys89 <s.sdr.roy89@gmial.com>
2024-01-23 18:12:18 +0530

    122

--- dashboard/admin-dashboard.php
+++ dashboard/admin-dashboard.php
@@ -231,61 +231,7 @@ if ($conn->connect_error) {
                                     </div>
                                 </div><!--end col-->
                             </div><!--end grid-->
-                            <div class="overflow-x-auto">
-                                <?php
-                                // Check connection
-                                    if ($conn->connect_error) {
-                                        die("Connection failed: " . $conn->connect_error);
-                                    }
-
-                                    // Query to fetch data for each unique user_id with game_type as "single"
-                                    $query = "SELECT
-                                                user_id,
-                                                phone,
-                                                baji,
-                                                SUM(amount) AS total_amount,
-                                                GROUP_CONCAT(bet_number ORDER BY bet_number ASC) AS bet_numbers
-                                                COUNT(b.bet_number) AS total_bets
-                                            FROM bet_table
-                                            WHERE game_type = 'single'
-                                            GROUP BY user_id";
-
-                                    $result = $conn->query($query); 
-                                    if ($result->num_rows > 0) {
-                                        echo
-                               '<table class="w-full whitespace-nowrap">
-                                    <thead class="ltr:text-left rtl:text-right bg-slate-100 text-slate-500 dark:text-zink-200 dark:bg-zink-600">
-                                        
-                                        <tr>
-                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Phone</th>
-                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Amount</th>
-                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Bet No.</th>
-                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Baji</th>
-                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Total Bet</th>
-                                        </tr>
-                                    </thead>';
-                                    while ($row = $result->fetch_assoc()) {
-                                        echo '
-                                    <tbody>
-                                        <tr>
-                                            <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500"><a href="apps-ecommerce-order-overview.html">'. $row['phone_no'] .'</a></td>
-                                            <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">'. $row['total_amount'] .'</td>
-                                            <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">'. $row['bet_numbers'] .'</td>
-                                            <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">'. $row['baji'] .'</td>
-                                            <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">'. $row['total_bets'] .'</td>
-                                            </td>
-                                        </tr>
-                                    </tbody>';
-                                    }
-                               echo '</table>';
-                            } else {
-                                echo "No data found";
-                            }
                             
-                            // Close the database connection
-                            $conn->close();
-                                ?>
-                            </div>
                         </div>
                     </div><!--end col-->
                     <div class="col-span-12 card lg:col-span-6 2xl:col-span-3">
