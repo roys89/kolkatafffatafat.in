314ef808a8ebed8a6bdac32e281b5962c9f331e6
roys89 <s.sdr.roy89@gmial.com>
2024-01-23 17:02:23 +0530

    110

--- dashboard/admin-login.php
+++ dashboard/admin-login.php
@@ -1,12 +1,64 @@
+<?php
+session_start();
+// Check if the user is already logged in
+if (isset($_SESSION['admin_id'])) {
+  header("Location: admin-dashboard.php");
+  exit();
+}
+
+include 'database.php';
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form submission
+if ($adminPhone["REQUEST_METHOD"] == "POST") {
+
+    $adminPhone = $_POST["adminPhone"];
+    $adminPass = $_POST["adminPass"];
+
+    // Sanitize the inputs to prevent SQL injection
+    $adminPhone = mysqli_real_escape_string($conn, $adminPhone);
+    $adminPass = mysqli_real_escape_string($conn, $adminPass);
+
+
+
+    // Query to retrieve user information from the database
+    $query = "SELECT * FROM admin_data WHERE admin_phone='$adminPhone' AND admin_pass='$adminPass'";
+    $result = $conn->query($query);
+
+    // Check if a matching user is found
+    // Check if a matching user is found
+    if ($result->num_rows == 1) {
+        // Authentication successful, store user information in session
+        $_SESSION['admin_id'] = $username;
+        $_SESSION['admin_name'] = $adminName;
+        $_SESSION['admin_phone'] = $adminPhone;
+
+        // Redirect to a secure page
+        header('Location: admin-dashboard.php');
+        exit;
+    } else {
+        // Authentication failed, redirect back to the login page with an error message
+        header('Location: admin-login.php?error=1');
+        exit;
+    } 
+}
+
+// Close connection
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en" class="light scroll-smooth group" data-layout="vertical" data-sidebar="light" data-sidebar-size="lg" data-mode="light" data-topbar="light" data-skin="default" data-navbar="sticky" data-content="fluid" dir="ltr">
 
 
-<!-- Mirrored from themesdesign.in/tailwick/html/auth-login-boxed.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jan 2024 08:05:05 GMT -->
 <head>
 
     <meta charset="utf-8">
-    <title>Sign In | Tailwick - Admin & Dashboard Template</title>
+    <title>Sign In Kolkata FF Live Admin Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta content="Minimal Admin & Dashboard Template" name="description">
     <meta content="Themesdesign" name="author">
@@ -16,7 +68,7 @@
     <script src="assets/js/layout.js"></script>
     <!-- Icons CSS -->
     
-    <!-- Tailwind CSS -->
+
     
 
   <link rel="stylesheet" href="assets/css/tailwind2.css">
@@ -31,21 +83,21 @@
                     
                     <div class="text-center">
                         <h4 class="mb-2 text-purple-500 dark:text-purple-500">Welcome Back !</h4>
-                        <p class="text-slate-500 dark:text-zink-200">Sign in to continue to Tailwick.</p>
+                        <p class="text-slate-500 dark:text-zink-200">Sign in to continue to KOlkata FF Live.</p>
                     </div>
             
-                    <form action="#!" class="mt-10" id="signInForm">
+                    <form action="#!" class="mt-10" id="signInForm" method="POST">
                         <div class="hidden px-4 py-3 mb-3 text-sm text-green-500 border border-green-200 rounded-md bg-green-50 dark:bg-green-400/20 dark:border-green-500/50" id="successAlert">
                             You have <b>successfully</b> signed in.
                         </div>
                         <div class="mb-3">
-                            <label for="username" class="inline-block mb-2 text-base font-medium">UserName/ Email ID</label>
-                            <input type="text" id="username" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter username or email">
-                            <div id="username-error" class="hidden mt-1 text-sm text-red-500">Please enter a valid email address.</div>
+                            <label for="username" class="inline-block mb-2 text-base font-medium">Phone Number.</label>
+                            <input type="text" id="adminPhone" name="adminPhone" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter username or email">
+                            <div id="username-error" class="hidden mt-1 text-sm text-red-500">Please enter a valid Phone Number.</div>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="inline-block mb-2 text-base font-medium">Password</label>
-                            <input type="password" id="password" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter password">
+                            <input type="password" id="admin_pass" name="admin_pass" class="form-input dark:bg-zink-600/50 border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Enter password">
                             <div id="password-error" class="hidden mt-1 text-sm text-red-500">Password must be at least 8 characters long and contain both letters and numbers.</div>
                         </div>
                         <div>
@@ -59,19 +111,13 @@
                             <button type="submit" class="w-full text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">Sign In</button>
                         </div>
             
-                        <div class="relative text-center my-9 before:absolute before:top-3 before:left-0 before:right-0 before:border-t before:border-t-slate-200 dark:before:border-t-zink-500">
-                            <h5 class="inline-block px-2 py-0.5 text-sm bg-white text-slate-500 dark:bg-zink-600 dark:text-zink-200 rounded relative">Sign In with</h5>
-                        </div>
+
             
                         <div class="flex flex-wrap justify-center gap-2">
                             <button type="button" class="flex items-center justify-center w-[37.5px] h-[37.5px] transition-all duration-200 ease-linear p-0 text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 active:text-white active:bg-custom-600 active:border-custom-600"><i data-lucide="facebook" class="w-4 h-4"></i></button>
                             <button type="button" class="flex items-center justify-center w-[37.5px] h-[37.5px] transition-all duration-200 ease-linear p-0 text-white btn bg-orange-500 border-orange-500 hover:text-white hover:bg-orange-600 hover:border-orange-600 focus:text-white focus:bg-orange-600 focus:border-orange-600 active:text-white active:bg-orange-600 active:border-orange-600"><i data-lucide="mail" class="w-4 h-4"></i></button>
                             <button type="button" class="flex items-center justify-center w-[37.5px] h-[37.5px] transition-all duration-200 ease-linear p-0 text-white btn bg-sky-500 border-sky-500 hover:text-white hover:bg-sky-600 hover:border-sky-600 focus:text-white focus:bg-sky-600 focus:border-sky-600 active:text-white active:bg-sky-600 active:border-sky-600"><i data-lucide="twitter" class="w-4 h-4"></i></button>
                             <button type="button" class="flex items-center justify-center w-[37.5px] h-[37.5px] transition-all duration-200 ease-linear p-0 text-white btn bg-slate-500 border-slate-500 hover:text-white hover:bg-slate-600 hover:border-slate-600 focus:text-white focus:bg-slate-600 focus:border-slate-600 active:text-white active:bg-slate-600 active:border-slate-600"><i data-lucide="github" class="w-4 h-4"></i></button>
-                        </div>
-            
-                        <div class="mt-10 text-center">
-                            <p class="mb-0 text-slate-500 dark:text-zink-200">Don't have an account ? <a href="auth-signup-cover.html" class="font-semibold underline transition-all duration-150 ease-linear text-slate-500 dark:text-zink-200 hover:text-custom-500 dark:hover:text-custom-500"> SignUp</a> </p>
                         </div>
                     </form>
                 </div>
@@ -148,6 +194,4 @@
 
 </body>
 
-
-<!-- Mirrored from themesdesign.in/tailwick/html/auth-login-boxed.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jan 2024 08:05:05 GMT -->
 </html>
