04dfc36c91a2f9252a49b1e27e09440f9e05eaf6
roys89 <s.sdr.roy89@gmial.com>
2024-01-25 23:13:35 +0530

    1

--- dashboard/forms-basic.php
+++ dashboard/forms-basic.php
@@ -6,7 +6,7 @@
 <head>
 
     <meta charset="utf-8">
-    <title>Basic Elements | Tailwick - Admin & Dashboard Template</title>
+    <title>Result Submit- Kolkata FF Live</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta content="Minimal Admin & Dashboard Template" name="description">
     <meta content="Themesdesign" name="author">
@@ -175,7 +175,7 @@
 
                 <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
                     <div class="grow">
-                        <h5 class="text-16">Basic Elements</h5>
+                        <h5 class="text-16">Patti Result</h5>
                     </div>
                     <ul class="flex items-center gap-2 text-sm font-normal shrink-0">
                         <li class="relative before:content-['\ea54'] before:font-remix ltr:before:-right-1 rtl:before:-left-1  before:absolute before:text-[18px] before:-top-[3px] ltr:pr-4 rtl:pl-4 before:text-slate-400 dark:text-zink-200">
@@ -221,7 +221,7 @@
 
                 <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
                     <div class="grow">
-                        <h5 class="text-16">Basic Elements</h5>
+                        <h5 class="text-16">Single Result</h5>
                     </div>
                     <ul class="flex items-center gap-2 text-sm font-normal shrink-0">
                         <li class="relative before:content-['\ea54'] before:font-remix ltr:before:-right-1 rtl:before:-left-1  before:absolute before:text-[18px] before:-top-[3px] ltr:pr-4 rtl:pl-4 before:text-slate-400 dark:text-zink-200">
