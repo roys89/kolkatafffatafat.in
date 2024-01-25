a53c50872de607013c11df97cf1e3f440e0cda86
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 04:58:27 +0530

    1

--- dashboard/forms-basic.php
+++ dashboard/forms-basic.php
@@ -197,11 +197,11 @@
                                         include '../database.php';
                                         // Fetch user IDs from the user_data table
                                         $result = $conn->query("SELECT phone FROM user_data");
-                                        
+                                            echo "<option selected>Select Baji</option>";
                                         // Display user IDs in the dropdown
                                         while ($row = $result->fetch_assoc()) {
 
-                                            echo "<option selected>Select Baji</option>
+                                            echo "
                                                   <option value='{$row['phone']}'>{$row['phone']}</option>";
                                                 }
 
@@ -212,7 +212,7 @@
                                 </div>
                                 <div class="mb-3">
                                     <select id="baji" name="baji" class="form-select border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200">
-                                        <option selected>Select Baji</option>
+                                        <option selected>Select Status</option>
                                         <option value="active">Active</option>
                                         <option value="inactive">Inactive</option>
                                     </select>
