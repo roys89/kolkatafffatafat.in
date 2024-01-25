c76cf942b08ec00fa083b984a7cc58f5fef39dd8
roys89 <s.sdr.roy89@gmial.com>
2024-01-25 23:07:12 +0530

    1

--- dashboard/forms-basic.php
+++ dashboard/forms-basic.php
@@ -194,7 +194,7 @@
                                     <label for="inputText" class="inline-block mb-2 text-base font-medium">Result <span class="text-red-500">*</span></label>
                                     <input type="text" id="single_result" name="single_result" class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" required>
                                 </div>
-                                <div>
+                                <div class="mb-3">
                                     <select class="form-select border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200">
                                         <option selected>Open this select menu</option>
                                         <option value="1">One</option>
@@ -202,7 +202,6 @@
                                         <option value="3">Three</option>
                                     </select>
                                 </div>
-                                <br>
                                 <button type="submit" class="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">Submit</button>
                             </form>
                         </div>
