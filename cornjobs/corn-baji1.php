a33ff38b7537e54eb08f2beb3f66a24bbac968e1
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 03:22:45 +0530

    1

---
+++ cornjobs/corn-baji1.php
@@ -0,0 +1,16 @@
+<?php
+// Include your database connection file
+include '../database.php';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Update records in game_table where closing_time is less than or equal to the current server time
+$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 1";
+$result = $conn->query($query);
+
+// Close the database connection
+$conn->close();
+?>
