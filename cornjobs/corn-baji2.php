28d9ad0240faf8f3d5ad688a7547638d096f136c
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 03:26:55 +0530

    1

--- cornjobs/corn-baji2.php
+++ cornjobs/corn-baji2.php
@@ -8,11 +8,9 @@ if ($conn->connect_error) {
 }
 
 // Update records in game_table where closing_time is less than or equal to the current server time
-$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 1";
+$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 2";
 $result = $conn->query($query);
 
 // Close the database connection
 $conn->close();
 ?>
-
-domains/kolkatafffatafat.in/public_html/cornjobs/corn-baji1.php
