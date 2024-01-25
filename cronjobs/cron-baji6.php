2a8e4ba487f1322a5cfd74118b66391799d47e04
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 04:22:00 +0530

    1

--- cronjobs/cron-baji6.php
+++ cronjobs/cron-baji6.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 }
 
 // Update records in game_table where closing_time is less than or equal to the current server time
-$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 1";
+$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 6";
 $result = $conn->query($query);
 
 if (!$result) {
--- cronjobs/cron-truncate.php
+++ cronjobs/cron-truncate.php
@@ -12,8 +12,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Update records in game_table where closing_time is less than or equal to the current server time
-$query = "UPDATE game_table SET baji_status = 'close' WHERE baji = 1";
+// Truncate the baji_table
+$query = "TRUNCATE TABLE bet_table";
 $result = $conn->query($query);
 
 if (!$result) {
@@ -21,7 +21,7 @@ if (!$result) {
     die("Query failed: " . $conn->error);
 } else {
     // Output a success message or any other relevant information
-    echo "Records updated successfully.";
+    echo "bet_table truncated successfully.";
 }
 
 // Close the database connection
