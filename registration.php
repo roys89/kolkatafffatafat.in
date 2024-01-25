1e37bca369d073d7c57c25b10b386665765d0498
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 02:29:58 +0530

    1

--- registration.php
+++ registration.php
@@ -15,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $refId = $_POST["ref_id"];
     $loginPassword = $_POST["login_password"];
     $confirm_password = $_POST["c_password"];
+    $user_status = $_POST['active'];
 
     if ($loginPassword !== $confirm_password) {
         echo '<script>alert("Passwords do not match!");</script>';
@@ -34,11 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($check_stmt->num_rows > 0) {
             echo '<script>alert("Mobile number or email is already in use!");</script>';
         } else {
-            $insert_query = "INSERT INTO user_data (user_id, full_name, email, phone, ref_id, hashed_password, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $insert_query = "INSERT INTO user_data (user_id, full_name, email, phone, ref_id, hashed_password, password, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             $insert_stmt = $conn->prepare($insert_query);
 
             if ($insert_stmt) {
-                $insert_stmt->bind_param("sssssss",$user_id, $fullName, $email, $phone, $refId, $hashedPassword, $loginPassword);
+                $insert_stmt->bind_param("ssssssss",$user_id, $fullName, $email, $phone, $refId, $hashedPassword, $loginPassword, $user_status);
 
                 if ($insert_stmt->execute()) {
                     $_SESSION['user_id'] = $user_id;
