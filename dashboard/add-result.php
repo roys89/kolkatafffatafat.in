545b46c53b5a2308f97c856b8f1ebdc9bc8d7af6
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 21:53:24 +0530

    1

--- dashboard/add-result.php
+++ dashboard/add-result.php
@@ -6,24 +6,11 @@ include '../database.php';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-function sumOfDigits($number) {
-    // Convert the number to a string to iterate through each digit
-    $numberAsString = (string)$number;
 
-    // Initialize a variable to store the sum
-    $sum = 0;
-
-    // Iterate through each digit and add it to the sum
-    for ($i = 0; $i < strlen($numberAsString); $i++) {
-        $sum += (int)$numberAsString[$i];
-    }
-
-    return $sum;
-}
 // Get user input from the form
 $userChoice = $_POST['baji'];
 $column1 = $_POST['single_result'];
-$column2 = sumOfDigits($column1);
+
 // Get other input fields as needed
 
 // Validate and sanitize user input here (consider using prepared statements)
@@ -36,8 +23,7 @@ $conn->begin_transaction();
 
 // Update game_table
 $sqlGameTable = "UPDATE game_table
-                 SET patti_result = '$column1',
-                 single_result = '$column2' 
+                 SET patti_result = '$column1'
                  WHERE baji = '$userChoice'";
 
 if (!$conn->query($sqlGameTable)) {
