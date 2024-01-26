29537557501d00e2f025fceac3c663ee17186763
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 22:05:37 +0530

    1

--- dashboard/add-result.php
+++ dashboard/add-result.php
@@ -6,24 +6,10 @@ include '../database.php';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-function sumOfDigits($number) {
-    // Convert the number to a string to iterate through each digit
-    $numberAsString = (string)$number;
 
-    // Initialize a variable to store the sum
-    $sum = 0;
-
-    // Iterate through each digit and add it to the sum
-    for ($i = 0; $i < strlen($numberAsString); $i++) {
-        $sum += (int)$numberAsString[$i];
-    }
-
-    return $sum;
-}
 // Get user input from the form
 $userChoice = $_POST['baji'];
 $column1 = $_POST['single_result'];
-$column2 = sumOfDigits($column1);
 // Get other input fields as needed
 
 // Validate and sanitize user input here (consider using prepared statements)
@@ -36,8 +22,7 @@ $conn->begin_transaction();
 
 // Update game_table
 $sqlGameTable = "UPDATE game_table
-                 SET patti_result = '$column1',
-                 single_result = '$column2' 
+                 SET single_result = '$column1'
                  WHERE baji = '$userChoice'";
 
 if (!$conn->query($sqlGameTable)) {
@@ -57,26 +42,10 @@ $sqlUserTable = "UPDATE user_data
                      WHERE bet_number = '$column1' AND baji = '$userChoice'
                  )";
 
-$sqlUserTable2 = "UPDATE user_data
-                SET wallet_bal = wallet_bal + (10 * (
-                    SELECT SUM(amount)
-                    FROM bet_table
-                    WHERE bet_number = '$column2' AND baji = '$userChoice'
-                ))
-                WHERE user_id IN (
-                    SELECT user_id
-                    FROM bet_table
-                    WHERE bet_number = '$column2' AND baji = '$userChoice'
-                )";
-
 
 if (!$conn->query($sqlUserTable)) {
     $success = false;
 }
-if (!$conn->query($sqlUserTable2)) {
-    $success = false;
-}
-
 
 // Commit the transaction if both queries are successful, otherwise, rollback
 if ($success) {
