3115cc39c38274631b91a56a338483fd25599135
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 21:24:38 +0530

    1

--- dashboard/forms-basic.php
+++ dashboard/forms-basic.php
@@ -2,7 +2,7 @@
 <html lang="en" class="light scroll-smooth group" data-layout="vertical" data-sidebar="light" data-sidebar-size="lg" data-mode="light" data-topbar="light" data-skin="default" data-navbar="sticky" data-content="fluid" dir="ltr">
 
 
-<!-- Mirrored from themesdesign.in/tailwick/html/forms-basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jan 2024 08:05:28 GMT -->
+
 <head>
 
     <meta charset="utf-8">
@@ -192,7 +192,7 @@
                 <div class="grid grid-cols-1 gap-x-5 xl:grid-cols-3">
                     <div class="card">
                         <div class="card-body">
-                            <form action="pattiresult-submit.php" method="post">
+                            <form action="add-result.php" method="post">
                                 <div class="mb-3">
                                     <label for="inputText" class="inline-block mb-2 text-base font-medium">Result <span class="text-red-500">*</span></label>
                                     <input type="text" id="single_result" name="single_result" class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" required>
@@ -458,5 +458,5 @@
 </body>
 
 
-<!-- Mirrored from themesdesign.in/tailwick/html/forms-basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jan 2024 08:05:28 GMT -->
+
 </html>
