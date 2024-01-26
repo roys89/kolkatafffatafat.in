34672a026301a208e6b8aba6b1cb0616d2aeca37
roys89 <s.sdr.roy89@gmial.com>
2024-01-26 18:59:29 +0530

    1

--- dashboard/user-data.php
+++ dashboard/user-data.php
@@ -546,14 +546,14 @@ if (!isset($_SESSION['admin_id'])) {
 
                             <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
                                 <div class="grow">
-                                    <h5 class="text-16">Add Wallet Balence</h5>
+                                    <h5 class="text-16">Remove Wallet Balence</h5>
                                 </div>
                                 <ul class="flex items-center gap-2 text-sm font-normal shrink-0">
                                     <li class="relative before:content-['\ea54'] before:font-remix ltr:before:-right-1 rtl:before:-left-1  before:absolute before:text-[18px] before:-top-[3px] ltr:pr-4 rtl:pl-4 before:text-slate-400 dark:text-zink-200">
                                         <a href="#!" class="text-slate-400 dark:text-zink-200">Forms</a>
                                     </li>
                                     <li class="text-slate-700 dark:text-zink-100">
-                                    Add Wallet Balence
+                                    Remove Wallet Balence
                                     </li>
                                 </ul>
                             </div>
