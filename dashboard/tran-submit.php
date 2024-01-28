707a14e106b7572e91484e4047a3b2987ae3da23
roys89 <s.sdr.roy89@gmial.com>
2024-01-28 14:00:53 +0530

    1

--- dashboard/tran-submit.php
+++ dashboard/tran-submit.php
@@ -17,13 +17,6 @@ if ($status === 'approved' && $tran_id && $user_id && $transaction_request !== n
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Update wallet_bal in user_data table
-    $updateWalletQuery = "UPDATE user_data SET wallet_bal = wallet_bal + $transaction_request WHERE user_id = $user_id";
-    if ($conn->query($updateWalletQuery) === TRUE) {
-        echo '<script>console.log("Wallet update successful!");</script>';
-    } else {
-        echo '<script>console.error("Wallet update failed: ' . $conn->error . '");</script>';
-    }
 
     // Update transaction_status in transaction_table
     $updateStatusQuery = "UPDATE transaction_table SET transaction_status = 'approved' WHERE tran_id = $tran_id";
