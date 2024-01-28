2ea16ab5e5a48f7fa8dd3280dcd9e1ab206f9587
roys89 <s.sdr.roy89@gmial.com>
2024-01-28 13:52:16 +0530

    1

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction Details</title>
+</head>
+<body>
+
 <?php
 
 // Retrieve parameters from the URL
@@ -7,10 +16,13 @@ $transaction_request = $_GET['transaction_request'] ?? null;
 $status = $_GET['status'] ?? null;
 ?>
 
-<h1><?php echo $tran_id; ?></h1>
+<h1>Transaction ID: <?php echo $tran_id; ?></h1>
 
-<h1><?php echo $user_id; ?></h1>
+<h1>User ID: <?php echo $user_id; ?></h1>
 
-<h1><?php echo $transaction_request; ?></h1>
+<h1>Transaction Request: <?php echo $transaction_request; ?></h1>
 
-<h1><?php echo $status; ?></h1>
+<h1>Status: <?php echo $status; ?></h1>
+
+</body>
+</html>
