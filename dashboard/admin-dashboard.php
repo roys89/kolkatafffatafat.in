45f6bfbfcf3e540b5a539d5a186605be2366d3ac
roys89 <s.sdr.roy89@gmial.com>
2024-01-28 11:59:15 +0530

    1

--- dashboard/admin-dashboard.php
+++ dashboard/admin-dashboard.php
@@ -240,38 +240,42 @@ $totalAmount = $betAmountResult->fetch_assoc()['total_amount'];
                     </ul>
                 </div>
 
+
+            <div class="relative min-h-screen group-data-[sidebar-size=sm]:min-h-sm">
+
+            <div class="group-data-[sidebar-size=lg]:ltr:md:ml-vertical-menu group-data-[sidebar-size=lg]:rtl:md:mr-vertical-menu group-data-[sidebar-size=md]:ltr:ml-vertical-menu-md group-data-[sidebar-size=md]:rtl:mr-vertical-menu-md group-data-[sidebar-size=sm]:ltr:ml-vertical-menu-sm group-data-[sidebar-size=sm]:rtl:mr-vertical-menu-sm pt-[calc(theme('spacing.header')_*_1)] pb-[calc(theme('spacing.header')_*_0.8)] px-4 group-data-[navbar=bordered]:pt-[calc(theme('spacing.header')_*_1.3)] group-data-[navbar=hidden]:pt-0 group-data-[layout=horizontal]:mx-auto group-data-[layout=horizontal]:max-w-screen-2xl group-data-[layout=horizontal]:px-0 group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:ltr:md:ml-auto group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:rtl:md:mr-auto group-data-[layout=horizontal]:md:pt-[calc(theme('spacing.header')_*_1.8)] group-data-[layout=horizontal]:px-3 group-data-[layout=horizontal]:group-data-[navbar=hidden]:pt-[calc(theme('spacing.header')_*_0.9)]">        
+            <div class="container-fluid group-data-[content=boxed]:max-w-boxed mx-auto">
                 <div class="grid grid-cols-1 gap-x-5 xl:grid-cols-3">
-                    <div class="container mt-5">
+                        <div class="container mt-5">
                             <div class="row">
-                            <!-- Card 1 -->
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo number_format($totalWalletBal, 2); ?></h5>
-                                    <p class="card-text">Wallet Balance </p>
+                                <!-- Card 1 -->
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo number_format($totalWalletBal, 2); ?></h5>
+                                        <p class="card-text">Wallet Balance </p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <!-- Card 2 -->
-                            <div class="card">
-                                <div class="card-body">
-                                        <h5 class="card-title"><?php echo $totalPhones; ?></h5>
-                                        <p class="card-text">Users</p>
+                                <!-- Card 2 -->
+                                <div class="card">
+                                    <div class="card-body">
+                                            <h5 class="card-title"><?php echo $totalPhones; ?></h5>
+                                            <p class="card-text">Users</p>
+                                    </div>
+                                </div>
+                                <!-- Card 3 -->
+                                <div class="card">
+                                    <div class="card-body">
+                                    <h5 class="card-title"><?php echo number_format($totalAmount, 2); ?></h5>
+                                            <p class="card-text">Bet Placed Amount</p>
+                                    </div>
                                 </div>
                             </div>
-                            <!-- Card 3 -->
-                            <div class="card">
-                                <div class="card-body">
-                                <h5 class="card-title"><?php echo number_format($totalAmount, 2); ?></h5>
-                                        <p class="card-text">Bet Placed Amount</p>
-                                </div>
-                            </div>
-                    </div>
-                </div> 
-
-                    
-                    
-                </div><!--end grid-->
-                
+                        </div> 
+                  </div><!--end grid-->
+            </div>
+            </div>
+            </div>
 
                 <div class="grid grid-cols-12 2xl:grid-cols-12 gap-x-5">
                 <div class="relative col-span-12 overflow-hidden card 2xl:col-span-8 bg-slate-900">
