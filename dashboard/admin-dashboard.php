fcece9b0abd4a41d5ea488fc7d27657ee4592d21
roys89 <s.sdr.roy89@gmial.com>
2024-01-28 11:39:18 +0530

    1

--- dashboard/admin-dashboard.php
+++ dashboard/admin-dashboard.php
@@ -194,7 +194,11 @@ if (!isset($_SESSION['admin_id'])) {
     </header>
     
     
-                <div class="container mt-5">
+                
+
+    <div class="relative min-h-screen group-data-[sidebar-size=sm]:min-h-sm">
+
+    <div class="container mt-5">
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card">
@@ -224,8 +228,6 @@ if (!isset($_SESSION['admin_id'])) {
                     </div>
                 </div>
                 </div>
-
-    <div class="relative min-h-screen group-data-[sidebar-size=sm]:min-h-sm">
 
         <div class="group-data-[sidebar-size=lg]:ltr:md:ml-vertical-menu group-data-[sidebar-size=lg]:rtl:md:mr-vertical-menu group-data-[sidebar-size=md]:ltr:ml-vertical-menu-md group-data-[sidebar-size=md]:rtl:mr-vertical-menu-md group-data-[sidebar-size=sm]:ltr:ml-vertical-menu-sm group-data-[sidebar-size=sm]:rtl:mr-vertical-menu-sm pt-[calc(theme('spacing.header')_*_1)] pb-[calc(theme('spacing.header')_*_0.8)] px-4 group-data-[navbar=bordered]:pt-[calc(theme('spacing.header')_*_1.3)] group-data-[navbar=hidden]:pt-0 group-data-[layout=horizontal]:mx-auto group-data-[layout=horizontal]:max-w-screen-2xl group-data-[layout=horizontal]:px-0 group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:ltr:md:ml-auto group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:rtl:md:mr-auto group-data-[layout=horizontal]:md:pt-[calc(theme('spacing.header')_*_1.8)] group-data-[layout=horizontal]:px-3 group-data-[layout=horizontal]:group-data-[navbar=hidden]:pt-[calc(theme('spacing.header')_*_0.9)]">
             <div class="container-fluid group-data-[content=boxed]:max-w-boxed mx-auto">
