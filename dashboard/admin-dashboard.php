0ff34e1ed33a40623de38f2b3e6f02947c71a488
roys89 <s.sdr.roy89@gmial.com>
2024-01-29 23:57:08 +0530

    1

--- dashboard/admin-dashboard.php
+++ dashboard/admin-dashboard.php
@@ -417,7 +417,7 @@ $totalAmount = $betAmountResult->fetch_assoc()['total_amount'];
 
                     <div class="col-span-12 card 2xl:col-span-12">
                         <div class="card-body">
-                            <div class="grid items-center grid-cols-1 gap-3 mb-5 2xl:grid-cols-12">
+                            <div class="grid items-center grid-cols-1 gap-3 mb-5 2xl:grid-cols-6">
                                 <div class="2xl:col-span-3">
                                     <h6 class="text-15">Single List</h6> 
                                 </div><!--end col-->
@@ -475,7 +475,7 @@ $totalAmount = $betAmountResult->fetch_assoc()['total_amount'];
                         </div>
 
                         <div class="card-body">
-                            <div class="grid items-center grid-cols-1 gap-3 mb-5 2xl:grid-cols-12">
+                            <div class="grid items-center grid-cols-1 gap-3 mb-5 2xl:grid-cols-6">
                                 <div class="2xl:col-span-3">
                                     <h6 class="text-15">Patti List</h6> 
                                 </div><!--end col-->
@@ -505,8 +505,7 @@ $totalAmount = $betAmountResult->fetch_assoc()['total_amount'];
                                                         <tr>
                                                             <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Number</th>
                                                             <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Total Amount</th>
-                                                            <th class="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-y border-slate-200 dark:border-zink-500">Total Bets</th>
-                                                        </tr>
+                                                            </tr>
                                                     </thead>';
                                                     while ($row = $result->fetch_assoc()) {
                                                         $bet_number = $row['bet_number'];
@@ -517,23 +516,7 @@ $totalAmount = $betAmountResult->fetch_assoc()['total_amount'];
                                                                     <tr>
                                                                         <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500"><a href="'.$url2.'">' . $bet_number. '</a></td>
                                                                         <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $total_amount . '</td>
-                                                                        <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $total_bets . '</td>
-                                                                        <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">
-                                                                            <div class="relative dropdown">
-                                                                                <button id="orderAction1" data-bs-toggle="dropdown" class="flex items-center justify-center w-[30px] h-[30px] dropdown-toggle p-0 text-slate-500 btn bg-slate-100 hover:text-white hover:bg-slate-600 focus:text-white focus:bg-slate-600 focus:ring focus:ring-slate-100 active:text-white active:bg-slate-600 active:ring active:ring-slate-100 dark:bg-slate-500/20 dark:text-slate-400 dark:hover:bg-slate-500 dark:hover:text-white dark:focus:bg-slate-500 dark:focus:text-white dark:active:bg-slate-500 dark:active:text-white dark:ring-slate-400/20"><i data-lucide="more-horizontal" class="w-3 h-3"></i></button>
-                                                                                <ul class="absolute z-50 hidden py-2 mt-1 ltr:text-left rtl:text-right list-none bg-white rounded-md shadow-md dropdown-menu min-w-[10rem] dark:bg-zink-600" aria-labelledby="orderAction1">
-                                                                                    <li>
-                                                                                        <a class="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" href="'.$url2.'"><i data-lucide="eye" class="inline-block w-3 h-3 ltr:mr-1 rtl:ml-1"></i> <span class="align-middle">Overview</span></a>
-                                                                                    </li>
-                                                                                    <li>
-                                                                                        <a class="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" href="#!"><i data-lucide="file-edit" class="inline-block w-3 h-3 ltr:mr-1 rtl:ml-1"></i> <span class="align-middle">Edit</span></a>
-                                                                                    </li>
-                                                                                    <li>
-                                                                                        <a class="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" href="#!"><i data-lucide="trash-2" class="inline-block w-3 h-3 ltr:mr-1 rtl:ml-1"></i> <span class="align-middle">Delete</span></a>
-                                                                                    </li>
-                                                                                </ul>
-                                                                            </div>
-                                                                        </td>
+                                                                        
                                                                     </tr>
                                                                 </tbody>';
                                                             }
