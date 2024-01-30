12030a4d39ae4b808e0ee96f5198b95781009865
roys89 <s.sdr.roy89@gmial.com>
2024-01-30 14:29:40 +0530

    1

--- dashboard/all-bets.php
+++ dashboard/all-bets.php
@@ -408,6 +408,7 @@ $baji = urldecode($_GET['baji']);
                     <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $row['amount'] . '</td>
                     <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $row['game_type'] . '</td>
                     <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $row['result_status'] . '</td>
+                    <td class="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500">' . $row['bid_timestamp'] . '</td>
                 </tr>
             </tbody>';
                                         }
