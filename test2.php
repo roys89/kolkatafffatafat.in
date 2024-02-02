4d38eef5d4324adda8cd7a76515da42c263f01fb
roys89 <s.sdr.roy89@gmial.com>
2024-02-02 15:39:52 +0530

    1

--- test2.php
+++ test2.php
@@ -4,6 +4,5 @@ header('Content-Type: application/json');
 // Get server time
 $serverTime = date('Y-m-d H:i:s');
 
-// Return server time as JSON
 echo json_encode(['serverTime' => $serverTime]);
 ?>
