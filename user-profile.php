89ed5516b9e2b593f671b54d82ec771700be2179
roys89 <s.sdr.roy89@gmial.com>
2024-02-07 10:01:15 +0530

    1

--- user-profile.php
+++ user-profile.php
@@ -27,7 +27,7 @@ $user = $userResult->fetch_assoc();
 $userStmt->close();
 
 // Retrieve bet details from the bet_table for the logged-in user
-$Sql = "SELECT * FROM bet_table WHERE user_id = ?";
+$betSql = "SELECT * FROM bet_table WHERE user_id = ?";
 $betStmt = $conn->prepare($betSql);
 $betStmt->bind_param("i", $user_id);
 $betStmt->execute();
@@ -44,6 +44,7 @@ $transactionStmt->close();
 
 $conn->close();
 ?>
+
 
 
 <!DOCTYPE html>
