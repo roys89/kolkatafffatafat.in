6d11ea76cab488821ccca1204bf3aceac91f0f95
roys89 <s.sdr.roy89@gmial.com>
2024-02-17 22:29:33 +0530

    1

--- dashboard/add-result.php
+++ dashboard/add-result.php
@@ -32,9 +32,20 @@ if (!$conn->query($sqlGameTable)) {
 }
 
 // Update user_table
+$sqlUserTable = "UPDATE user_data
+                 SET wallet_bal = wallet_bal + (120 * (
+                     SELECT SUM(amount)
+                     FROM bet_table
+                     WHERE bet_number = '$column1' AND baji = '$userChoice'
+                 ))
+                 WHERE user_id IN (
+                     SELECT user_id
+                     FROM bet_table
+                     WHERE bet_number = '$column1' AND baji = '$userChoice'
+                 )";
 
 $sqlUserTable2 = "UPDATE user_data
-                SET wallet_bal = wallet_bal + (120 * (
+                SET wallet_bal = wallet_bal + (9.6 * (
                     SELECT SUM(amount)
                     FROM bet_table
                     WHERE bet_number = '$column2' AND baji = '$userChoice'
@@ -45,7 +56,9 @@ $sqlUserTable2 = "UPDATE user_data
                     WHERE bet_number = '$column2' AND baji = '$userChoice'
                 )";
 
-
+if (!$conn->query($sqlUserTable)) {
+    $success = false;
+}
 if (!$conn->query($sqlUserTable2)) {
     $success = false;
 }
