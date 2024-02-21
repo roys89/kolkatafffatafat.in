a33c557f0ae50bc2096013a9d261dc5392eb45cc
roys89 <s.sdr.roy89@gmial.com>
2024-02-21 16:07:56 +0530

    1

--- login.php
+++ login.php
@@ -217,7 +217,7 @@ if (isset($_SESSION['user_id'])) {
                     <div class="col-xl-6 col-lg-6">
                         <div class="reg-body login">
                             <form id="loginForm" action="models/processLogin.php" method="post">
-                                <input type="number" name="phone" id="phone" placeholder="Phone">
+                                <input type="text" name="phone" id="phone" placeholder="Phone">
                                 <input type="password" name="password" id="password" placeholder="Password">
                                 <div class="bottom-part">
                                     <div class="row">
