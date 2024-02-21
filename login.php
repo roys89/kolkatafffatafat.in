32f71e3ae4bd4cc3b3965be55a1831a8f9e1848b
roys89 <s.sdr.roy89@gmial.com>
2024-02-21 17:30:23 +0530

    1

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Check if the user is already logged in
-if (isset($_SESSION['id'])) {
+if (isset($_SESSION['user_id'])) {
     header("Location: user-profile.php");
     exit();
 }
