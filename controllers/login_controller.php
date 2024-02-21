eb707e797f061402f36976a54a074975480e4eeb
roys89 <s.sdr.roy89@gmial.com>
2024-02-21 13:35:02 +0530

    1

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require_once('models/login_model.php'); // Include the model for database operations
+    require_once('../models/login_model.php'); // Include the model for database operations
 
     $phone = filter_var($_POST['phone'], FILTER_SANITIZE_SPECIAL_CHARS);
     $loginPassword = filter_var($_POST['loginPassword'], FILTER_SANITIZE_SPECIAL_CHARS);
