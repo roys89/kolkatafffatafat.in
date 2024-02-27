efde22d4505cd04eaea70e553a0b3eb33b088e50
roys89 <s.sdr.roy89@gmial.com>
2024-02-27 16:43:16 +0530

    1

--- user-profile.php
+++ user-profile.php
@@ -247,8 +247,8 @@ $conn->close();
                 </ul>
 
                 <div class="act-buttons">
-                  <a href="user-add-money.php" class="follow">Add Bal<i class="far fa-eye"></a>
-                  <a href="user-withdraw.php" class="subscribe">Withdrow<i class="far fa-eye"></i></a>
+                  <a href="user-add-money.php" class="follow">Add Bal</a>
+                  <a href="user-withdraw.php" class="subscribe">Withdrow</i></a>
                   <a href="index.php" class="follow">Play</i></a>
                 
                   
